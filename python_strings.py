3ae8935bd9a4c163a0ca12781c6b557a85214f87
kdnmcntr <kaden.mcentire@gmail.com>
2023-01-30 19:16:55 -0700

    made some changes to the program

--- python_strings.py
+++ python_strings.py
@@ -8,8 +8,10 @@
 #       -set this equal to your birth year (doesn't have to be real should be less then 100 yrs ago)
 #   - current_year
 #       -set this equal to 2020
-
-
+firstName = 'Kaden'
+lastName = 'McEntire'
+birthYear = 2000
+currentYear = 2023
 
 
 # TODO String Indexing
@@ -20,7 +22,12 @@
 #       - second letter of your last name (use the -index)
 #       - first two letter of your first name (use the +index)
 #       - second two letter of your last name (use the -index)
-
+print (firstName)
+print (lastName)
+print (firstName[0])
+print (lastName[-2])
+print (firstName[0:2])
+print (lastName[-8:-6])
 
 
 
@@ -28,15 +35,18 @@
 #   - Print the following items (one per line) (print using variables)
 #       -first name and last name combined
 #       -first name six times
-
-
-
+print (firstName + " " + lastName)
+for i in range (6) :
+    print (firstName, end =" ")
+print ()
 
 
 # TODO Formatting Strings
 #   - Print the following items (one per line) (print using variables)
 #       - first name last name -was born in- year of birth
 #       - first name last name -was born in- year of birth. first name -enjoyed celebrating- current year
+print (firstName, lastName, "was born in", birthYear)
+print (firstName, lastName, "was born in", birthYear, ".", firstName, "enjoyed celebrating", currentYear)
 
 
 
@@ -44,7 +54,7 @@
 #   - Print the following items (one per line) (print using variables)
 #       - possesive first name -birth year is- year of birth 
 #       - tab last name current year
-
+print (firstName + "'s birth year is " + birthYear + "\t" + lastName + " " + currentYear)
 
 # TODO String methods
 #   - Print the following items (one per line) (print using variables)
